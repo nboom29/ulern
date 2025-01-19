2eff4471802ca383c1a5bcb30fbf8e3841d07bbb
nboom29 <idi_na_hui19@bk.ru>
2025-01-19 13:11:22 +0600

    color

--- project3/Program.cs
+++ project3/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace project3;
+﻿using System.Runtime.InteropServices;
+
+namespace project3;
 
 class Program
 {
+    static void Print(int color)
+    {
+        Console.BackgroundColor = (ConsoleColor)color;
+        Console.WriteLine("Hello, World!");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var color = 0;
+        
+        for (int i = 0; i < long.MaxValue; i++)
+        {
+            int maxValue = Enum.GetValues(typeof(ConsoleColor))
+                .Cast<int>()   // Преобразуем значения в int
+                .Max();        // Находим максимальное значение
+            if (color == maxValue)
+            {
+                Print(color);
+                color = 0;
+            }
+            else
+            {
+                Print(color);
+                color++;
+            }   
+
+        }
+       
     }
 }
